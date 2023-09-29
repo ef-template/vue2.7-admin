2fad8af0e42588b572d07363d325d7bbff40417d
fuguiyong <760720981@qq.com>
2023-09-29 15:30:26 +0800

    feat: 验证element-css-var

--- src/views/test/form.vue
+++ src/views/test/form.vue
@@ -1,84 +1,262 @@
-<script>
-export default {
-  data() {
-    return {
-      form: {
-        name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: "",
-      },
-    };
-  },
-  methods: {
-    onSubmit() {
-      // console.log('submit!')
-    },
-  },
+<script setup lang="ts">
+import { MessageBox, Message } from "element-ui";
+import { setThemeColor, getThemeColor } from "@/utils";
+
+const generateData = () => {
+  const data = [];
+  for (let i = 1; i <= 15; i++) {
+    data.push({
+      key: i,
+      label: `备选项 ${i}`,
+      disabled: i % 4 === 0,
+    });
+  }
+  return data;
 };
+
+const tdata = ref(generateData());
+const tvalue = ref([1, 4]);
+
+const form = reactive({
+  name: "",
+  region: "",
+  date1: "",
+  date2: "",
+  delivery: false,
+  type: [],
+  resource: "",
+  desc: "",
+});
+
+const color1 = ref();
+
+const onSubmit = () => {};
+
+const open = () => {
+  MessageBox.alert("这是一段内容", "标题名称", {
+    confirmButtonText: "确定",
+    callback: (action) => {
+      Message({
+        type: "info",
+        message: `action: ${action}`,
+      });
+    },
+  });
+};
+
+const activeName = ref("first");
+const dialogVisible = ref(false);
+const handleClose = (done) => {
+  MessageBox.confirm("确认关闭？")
+    .then((_) => {
+      done();
+    })
+    .catch((_) => {});
+};
+
+const drawer = ref(false);
+const direction = ref("rtl");
+
+const handleColorChange = (color) => {
+  console.log({ color });
+  setThemeColor(color);
+};
+
+onMounted(() => {
+  console.log({
+    themeColor: getThemeColor(),
+  });
+});
 </script>
 
 <template>
-  <el-form :model="form" label-width="80px">
-    <el-form-item label="活动名称">
-      <el-input v-model="form.name" />
-    </el-form-item>
-    <el-form-item label="活动区域">
-      <el-select v-model="form.region" placeholder="请选择活动区域">
-        <el-option label="区域一" value="shanghai" />
-        <el-option label="区域二" value="beijing" />
-      </el-select>
-    </el-form-item>
-    <el-form-item label="活动时间">
-      <el-col :span="11">
-        <el-date-picker
-          v-model="form.date1"
-          type="date"
-          placeholder="选择日期"
-          style="width: 100%"
-        />
-      </el-col>
-      <el-col class="line" :span="2">
-        -
-      </el-col>
-      <el-col :span="11">
-        <el-time-picker
-          v-model="form.date2"
-          type="fixed-time"
-          placeholder="选择时间"
-          style="width: 100%"
-        />
-      </el-col>
-    </el-form-item>
-    <el-form-item label="即时配送">
-      <el-switch v-model="form.delivery" />
-    </el-form-item>
-    <el-form-item label="活动性质">
-      <el-checkbox-group v-model="form.type">
-        <el-checkbox label="美食/餐厅线上活动" name="type" />
-        <el-checkbox label="地推活动" name="type" />
-        <el-checkbox label="线下主题活动" name="type" />
-        <el-checkbox label="单纯品牌曝光" name="type" />
-      </el-checkbox-group>
-    </el-form-item>
-    <el-form-item label="特殊资源">
-      <el-radio-group v-model="form.resource">
-        <el-radio label="线上品牌商赞助" />
-        <el-radio label="线下场地免费" />
+  <div>
+    <div mt-20px>
+      <el-radio-group v-model="direction">
+        <el-radio label="ltr">从左往右开</el-radio>
+        <el-radio label="rtl">从右往左开</el-radio>
+        <el-radio label="ttb">从上往下开</el-radio>
+        <el-radio label="btt">从下往上开</el-radio>
       </el-radio-group>
-    </el-form-item>
-    <el-form-item label="活动形式">
-      <el-input v-model="form.desc" type="textarea" />
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="onSubmit">
-        立即创建
-      </el-button>
-      <el-button>取消</el-button>
-    </el-form-item>
-  </el-form>
+
+      <el-button @click="drawer = true" type="primary" style="margin-left: 16px;"> 点我打开 </el-button>
+      <!-- eslint-disable-next-line vue/no-deprecated-v-bind-sync -->
+      <el-drawer title="我是标题" :visible.sync="drawer" :direction="direction" :before-close="handleClose">
+        <span>我来啦!</span>
+      </el-drawer>
+    </div>
+    <div mt-20px>
+      <el-popconfirm title="这是一段内容确定删除吗？">
+        <el-button slot="reference">删除</el-button>
+      </el-popconfirm>
+    </div>
+    <div mt-20px>
+      <el-popover
+        placement="top-start"
+        title="标题"
+        width="200"
+        trigger="hover"
+        content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。"
+      >
+        <el-button slot="reference">hover 激活</el-button>
+      </el-popover>
+    </div>
+    <div class="box" mt-20px>
+      <div class="top">
+        <el-tooltip class="item" effect="dark" content="Top Left 提示文字" placement="top-start">
+          <el-button>上左</el-button>
+        </el-tooltip>
+        <el-tooltip class="item" effect="dark" content="Top Center 提示文字" placement="top">
+          <el-button>上边</el-button>
+        </el-tooltip>
+        <el-tooltip class="item" effect="dark" content="Top Right 提示文字" placement="top-end">
+          <el-button>上右</el-button>
+        </el-tooltip>
+      </div>
+      <div class="left">
+        <el-tooltip class="item" effect="dark" content="Left Top 提示文字" placement="left-start">
+          <el-button>左上</el-button>
+        </el-tooltip>
+        <el-tooltip class="item" effect="dark" content="Left Center 提示文字" placement="left">
+          <el-button>左边</el-button>
+        </el-tooltip>
+        <el-tooltip class="item" effect="dark" content="Left Bottom 提示文字" placement="left-end">
+          <el-button>左下</el-button>
+        </el-tooltip>
+      </div>
+
+      <div class="right">
+        <el-tooltip class="item" effect="dark" content="Right Top 提示文字" placement="right-start">
+          <el-button>右上</el-button>
+        </el-tooltip>
+        <el-tooltip class="item" effect="dark" content="Right Center 提示文字" placement="right">
+          <el-button>右边</el-button>
+        </el-tooltip>
+        <el-tooltip class="item" effect="dark" content="Right Bottom 提示文字" placement="right-end">
+          <el-button>右下</el-button>
+        </el-tooltip>
+      </div>
+      <div class="bottom">
+        <el-tooltip class="item" effect="dark" content="Bottom Left 提示文字" placement="bottom-start">
+          <el-button>下左</el-button>
+        </el-tooltip>
+        <el-tooltip class="item" effect="dark" content="Bottom Center 提示文字" placement="bottom">
+          <el-button>下边</el-button>
+        </el-tooltip>
+        <el-tooltip class="item" effect="dark" content="Bottom Right 提示文字" placement="bottom-end">
+          <el-button>下右</el-button>
+        </el-tooltip>
+      </div>
+    </div>
+    <div mt-20px>
+      <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
+      <!-- eslint-disable-next-line vue/no-deprecated-v-bind-sync -->
+      <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
+        <span>这是一段信息</span>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        </span>
+      </el-dialog>
+    </div>
+    <div mt-20px>
+      <el-dropdown>
+        <span class="el-dropdown-link"> 下拉菜单<i class="el-icon-arrow-down el-icon--right"></i> </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>黄金糕</el-dropdown-item>
+          <el-dropdown-item>狮子头</el-dropdown-item>
+          <el-dropdown-item>螺蛳粉</el-dropdown-item>
+          <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+          <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+    <div mt-20px>
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+        <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+        <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+        <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+      </el-tabs>
+    </div>
+    <div mt-20px>
+      <el-button type="text" @click="open">点击打开 Message Box</el-button>
+    </div>
+    <div mt-20px flex>
+      <el-badge :value="12" class="item" mr-20px>
+        <el-button size="small">评论</el-button>
+      </el-badge>
+      <el-badge :value="3" class="item" mr-20px>
+        <el-button size="small">回复</el-button>
+      </el-badge>
+      <el-badge :value="1" class="item" type="primary" mr-20px>
+        <el-button size="small">评论</el-button>
+      </el-badge>
+      <el-badge :value="2" class="item" type="warning">
+        <el-button size="small">回复</el-button>
+      </el-badge>
+    </div>
+    <div mt-20px>
+      <el-pagination background layout="prev, pager, next" :total="1000"> </el-pagination>
+    </div>
+    <div mt-20px>
+      <el-tag>标签一</el-tag>
+      <el-tag type="success">标签二</el-tag>
+      <el-tag type="info">标签三</el-tag>
+      <el-tag type="warning">标签四</el-tag>
+      <el-tag type="danger">标签五</el-tag>
+    </div>
+    <div mt-20px>
+      <el-transfer v-model="tvalue" :data="tdata"></el-transfer>
+    </div>
+    <div mt-20px>
+      <el-color-picker v-model="color1" @change="handleColorChange"></el-color-picker>
+    </div>
+    <div mt-20px>
+      <el-form :model="form" label-width="80px">
+        <el-form-item label="活动名称">
+          <el-input v-model="form.name" />
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai" />
+            <el-option label="区域二" value="beijing" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="活动时间">
+          <el-col :span="11">
+            <el-date-picker v-model="form.date1" type="date" placeholder="选择日期" style="width: 100%" />
+          </el-col>
+          <el-col class="line" :span="2"> - </el-col>
+          <el-col :span="11">
+            <el-time-picker v-model="form.date2" type="fixed-time" placeholder="选择时间" style="width: 100%" />
+          </el-col>
+        </el-form-item>
+        <el-form-item label="即时配送">
+          <el-switch v-model="form.delivery" />
+        </el-form-item>
+        <el-form-item label="活动性质">
+          <el-checkbox-group v-model="form.type">
+            <el-checkbox label="美食/餐厅线上活动" name="type" />
+            <el-checkbox label="地推活动" name="type" />
+            <el-checkbox label="线下主题活动" name="type" />
+            <el-checkbox label="单纯品牌曝光" name="type" />
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="特殊资源">
+          <el-radio-group v-model="form.resource">
+            <el-radio label="线上品牌商赞助" />
+            <el-radio label="线下场地免费" />
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="活动形式">
+          <el-input v-model="form.desc" type="textarea" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit"> 立即创建 </el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
--- src/views/test/icon.vue
+++ src/views/test/icon.vue
@@ -1,5 +1,310 @@
 <script setup lang="ts">
+const num = ref("");
+
+const value1 = ref()
+
+const handleChange = (val: string) => {
+  console.log({ val });
+};
+
+const options = ref([
+  {
+    value: "zhinan",
+    label: "指南",
+    disabled: true,
+    children: [
+      {
+        value: "shejiyuanze",
+        label: "设计原则",
+        children: [
+          {
+            value: "yizhi",
+            label: "一致",
+          },
+          {
+            value: "fankui",
+            label: "反馈",
+          },
+          {
+            value: "xiaolv",
+            label: "效率",
+          },
+          {
+            value: "kekong",
+            label: "可控",
+          },
+        ],
+      },
+      {
+        value: "daohang",
+        label: "导航",
+        children: [
+          {
+            value: "cexiangdaohang",
+            label: "侧向导航",
+          },
+          {
+            value: "dingbudaohang",
+            label: "顶部导航",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: "zujian",
+    label: "组件",
+    children: [
+      {
+        value: "basic",
+        label: "Basic",
+        children: [
+          {
+            value: "layout",
+            label: "Layout 布局",
+          },
+          {
+            value: "color",
+            label: "Color 色彩",
+          },
+          {
+            value: "typography",
+            label: "Typography 字体",
+          },
+          {
+            value: "icon",
+            label: "Icon 图标",
+          },
+          {
+            value: "button",
+            label: "Button 按钮",
+          },
+        ],
+      },
+      {
+        value: "form",
+        label: "Form",
+        children: [
+          {
+            value: "radio",
+            label: "Radio 单选框",
+          },
+          {
+            value: "checkbox",
+            label: "Checkbox 多选框",
+          },
+          {
+            value: "input",
+            label: "Input 输入框",
+          },
+          {
+            value: "input-number",
+            label: "InputNumber 计数器",
+          },
+          {
+            value: "select",
+            label: "Select 选择器",
+          },
+          {
+            value: "cascader",
+            label: "Cascader 级联选择器",
+          },
+          {
+            value: "switch",
+            label: "Switch 开关",
+          },
+          {
+            value: "slider",
+            label: "Slider 滑块",
+          },
+          {
+            value: "time-picker",
+            label: "TimePicker 时间选择器",
+          },
+          {
+            value: "date-picker",
+            label: "DatePicker 日期选择器",
+          },
+          {
+            value: "datetime-picker",
+            label: "DateTimePicker 日期时间选择器",
+          },
+          {
+            value: "upload",
+            label: "Upload 上传",
+          },
+          {
+            value: "rate",
+            label: "Rate 评分",
+          },
+          {
+            value: "form",
+            label: "Form 表单",
+          },
+        ],
+      },
+      {
+        value: "data",
+        label: "Data",
+        children: [
+          {
+            value: "table",
+            label: "Table 表格",
+          },
+          {
+            value: "tag",
+            label: "Tag 标签",
+          },
+          {
+            value: "progress",
+            label: "Progress 进度条",
+          },
+          {
+            value: "tree",
+            label: "Tree 树形控件",
+          },
+          {
+            value: "pagination",
+            label: "Pagination 分页",
+          },
+          {
+            value: "badge",
+            label: "Badge 标记",
+          },
+        ],
+      },
+      {
+        value: "notice",
+        label: "Notice",
+        children: [
+          {
+            value: "alert",
+            label: "Alert 警告",
+          },
+          {
+            value: "loading",
+            label: "Loading 加载",
+          },
+          {
+            value: "message",
+            label: "Message 消息提示",
+          },
+          {
+            value: "message-box",
+            label: "MessageBox 弹框",
+          },
+          {
+            value: "notification",
+            label: "Notification 通知",
+          },
+        ],
+      },
+      {
+        value: "navigation",
+        label: "Navigation",
+        children: [
+          {
+            value: "menu",
+            label: "NavMenu 导航菜单",
+          },
+          {
+            value: "tabs",
+            label: "Tabs 标签页",
+          },
+          {
+            value: "breadcrumb",
+            label: "Breadcrumb 面包屑",
+          },
+          {
+            value: "dropdown",
+            label: "Dropdown 下拉菜单",
+          },
+          {
+            value: "steps",
+            label: "Steps 步骤条",
+          },
+        ],
+      },
+      {
+        value: "others",
+        label: "Others",
+        children: [
+          {
+            value: "dialog",
+            label: "Dialog 对话框",
+          },
+          {
+            value: "tooltip",
+            label: "Tooltip 文字提示",
+          },
+          {
+            value: "popover",
+            label: "Popover 弹出框",
+          },
+          {
+            value: "card",
+            label: "Card 卡片",
+          },
+          {
+            value: "carousel",
+            label: "Carousel 走马灯",
+          },
+          {
+            value: "collapse",
+            label: "Collapse 折叠面板",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: "ziyuan",
+    label: "资源",
+    children: [
+      {
+        value: "axure",
+        label: "Axure Components",
+      },
+      {
+        value: "sketch",
+        label: "Sketch Templates",
+      },
+      {
+        value: "jiaohu",
+        label: "组件交互文档",
+      },
+    ],
+  },
+]);
 </script>
+
+<template>
+  <div>
+    <div>
+      <i class="el-icon-edit" />
+      <i class="el-icon-share" />
+      <i class="el-icon-delete" />
+      <el-button type="primary" icon="el-icon-search"> 搜索 </el-button>
+    </div>
+    <div mt-20px>
+      <el-link href="https://element.eleme.io" target="_blank">默认链接</el-link>
+      <el-link type="primary">主要链接</el-link>
+      <el-link type="success">成功链接</el-link>
+      <el-link type="warning">警告链接</el-link>
+      <el-link type="danger">危险链接</el-link>
+      <el-link type="info">信息链接</el-link>
+    </div>
+    <div mt-20px>
+      <el-input-number v-model="num" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
+    </div>
+    <div mt-20px>
+      <el-cascader :options="options"></el-cascader>
+    </div>
+    <div mt-20px>
+      <el-slider v-model="value1"></el-slider>
+    </div>
+  </div>
+</template>
 
 <route>
   {
@@ -8,14 +313,3 @@
     }
   }
 </route>
-
-<template>
-  <div>
-    <i class="el-icon-edit" />
-    <i class="el-icon-share" />
-    <i class="el-icon-delete" />
-    <el-button type="primary" icon="el-icon-search">
-      搜索
-    </el-button>
-  </div>
-</template>
